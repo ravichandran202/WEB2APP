41f1a7ee236ca034eb0d15b7daf97d8930f64c82
ravichandran202 <ravichandran202@gmail.com>
2023-09-01 20:00:55 +0530

    done with setup-templates and urls basics

--- web2app/web2androidapp/views.py
+++ web2app/web2androidapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def upload(request):
+    return render(request,'upload.html')
+
+def download(request):
+    return render(request,'download.html')
--- web2app/web2app/settings.py
+++ web2app/web2app/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMP_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = "web2app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMP_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
