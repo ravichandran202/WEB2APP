0445b12f3ef4501576e0f0be7a3a3daf691ce804
ravichandran202 <ravichandran202@gmail.com>
2023-09-02 09:52:09 +0530

    implimented authentication functionalities

--- web2app/web2androidapp/urls.py
+++ web2app/web2androidapp/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path("",views.home,name='home'),
     path("login",views.login,name='login'),
     path("signup",views.signup,name='signup'),
+    path("logout",views.logout,name='logout'),
     path("profile",views.profile,name='profile'),
     path("upload",views.upload,name='upload'),
     path("download",views.download,name='download'),
--- web2app/web2androidapp/views.py
+++ web2app/web2androidapp/views.py
@@ -1,15 +1,53 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request,'index.html')
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['name-field'].lower()
+        password = request.POST['password-field']
+        
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("home")
+        else:
+            messages.error(request,'Username or Password is incorrect')
+            return redirect("login")
     return render(request,'login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['name-field'].lower()
+        email = request.POST['email-field']
+        password1 = request.POST['password-field']
+        password2 = request.POST['password2-field']
+        if password1==password2:
+            if User.objects.filter(email=email).exists():
+                messages.error(request,"Email Already Exists")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.error(request,"username Already Exists")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username,email,password1)
+                user.save()
+                messages.info(request,"Account created successfully")
+                return redirect('login')
+        else:
+            messages.error(request,"Please Enter same Password")
+            return redirect('signup')
     return render(request,'signup.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
 
 def profile(request):
     return render(request,'profile.html')
